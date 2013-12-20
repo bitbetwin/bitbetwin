ea6577b90e1b16dee6d583803b72c4652eee9a70
Richard Holzeis <richard.holzeis@gmail.com>
2013-12-20 11:20:46 +0100

    enhanced e2e tests

--- public/js/test/e2e/hangman.e2e.js
+++ public/js/test/e2e/hangman.e2e.js
@@ -1,7 +1,41 @@
 'use strict';
 
 describe('HangmanCtrl', function() {
-  
+
+  beforeEach(function() {
+    browser.ignoreSynchronization = true;
+  });
+
+  it('should login', function() {
+    browser.get('http://localhost:8080/logout');
+
+    element(by.id('email')).sendKeys('user');
+    element(by.id('password')).sendKeys('password');
+
+    element(by.id('login-btn')).click();
+
+    var welcometext = element(by.id('welcome')).getText();
+    expect(welcometext).toEqual('You are currently logged in as user');
+    var greetingtext = element(by.id('greeting')).getText();
+    expect(greetingtext).toEqual('Bangman :) Hey: user');
+
+    var games = element.all(by.repeater('games'));
+    expect(games.count()).toEqual(1);
+    expect(games.get(0).getText()).toEqual('Join game1');
+  });
+
+  it('should register and be logged in afterwards', function() {
+    browser.get('http://localhost:8080/logout');
+
+    element(by.id('reg-email')).sendKeys('new-user');
+    element(by.id('reg-password')).sendKeys('password');
+
+    element(by.id('register-btn')).click();
+
+    var welcometext = element(by.id('welcome')).getText();
+    expect(welcometext).toEqual('You are currently logged in as new-user');
+  });
+
   it('should login and make a sample guess', function() {
     browser.get('http://localhost:8080/logout');
 
@@ -12,6 +46,11 @@ describe('HangmanCtrl', function() {
 
     var welcometext = element(by.id('welcome')).getText();
     expect(welcometext).toEqual('You are currently logged in as user');
+
+    var games = element.all(by.repeater('games'));
+    expect(games.count()).toEqual(1);
+    expect(games.get(0).getText()).toEqual('Join game1');
+    element(by.id('game1')).click();
 
     var matchtext = element(by.id('match')).getText();
     expect(matchtext).toEqual('Guessed Word: _______________ ___ _______ ___ ________ _________');
