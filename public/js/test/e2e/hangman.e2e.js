9fc97a03294f8bf70a9935ec5c8f03eb9356a1e6
Richard Holzeis <richard.holzeis@gmail.com>
2014-01-02 16:54:24 +0100

    added activation test

--- public/js/test/e2e/hangman.e2e.js
+++ public/js/test/e2e/hangman.e2e.js
@@ -29,17 +29,39 @@ describe('HangmanCtrl', function() {
     matchtext = element(by.id('match')).getText();
     expect(matchtext).toEqual('Guessed Word: T_________');
   });
-  
-  it('should register and show activation message afterwards', function() {
+
+  it('should register, activate account and sign in afterwards', function() {
     browser.get('http://localhost:8080/logout');
 
     element(by.id('reg-email')).sendKeys('new-user@gmail.com');
-    element(by.id('reg-password')).sendKeys('password');
+    element(by.id('reg-password')).sendKeys('passw0rd');
 
     element(by.id('register-btn')).click();
 
     var welcometext = element(by.id('info')).getText();
     expect(welcometext).toEqual('Please check your emails in order to activate your account new-user@gmail.com');
+    var activationtext = element(by.id('debug')).getText();
+    expect(activationtext).toContain("Please activate localhost:8080/activate?token=");
+
+    var activationurl = "---------- empty -----------";
+
+    element(by.id('debug')).getText().then(function(text) {
+      activationurl = "http://" + text.split(" ")[2];
+
+      console.log(activationurl);
+      browser.get(activationurl);
+
+      var signintext = element(by.id('info')).getText();
+      expect(signintext).toEqual('Please sign in new-user@gmail.com');
+
+      element(by.id('email')).sendKeys('new-user@gmail.com');
+      element(by.id('password')).sendKeys('passw0rd');
+
+      element(by.id('login-btn')).click();
+
+      welcometext = element(by.id('welcome')).getText();
+      expect(welcometext).toEqual('You are currently logged in as new-user@gmail.com');
+    });
   });
 
   it('should login', function() {
